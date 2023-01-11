206fab912ca72b56abc7e6f4c642d65ab724ac88
sakamuuy <sakamuu.y@gmail.com>
2023-01-11 23:10:37 +0900

    make ease

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::{f64::consts::PI, io::Write, thread, time};
 
 pub fn ease_in_sine(v: f64) -> f64 {
-    1.0 - (v * PI).cos() / 2.0
+    1.0 - ((v * PI) / 2.0).cos()
 }
 
 fn norm(value: f64, upper: f64, bottom: f64) -> f64 {
@@ -19,12 +19,11 @@ fn mapping(to_upper: f64, to_bottom: f64, from_upper: f64, from_bottom: f64, val
 fn print_with_easing(msg: &str) {
     let msg_len = msg.len();
     let mut printed_index: Vec<i32> = vec![];
-    let duration = time::Duration::from_millis(100);
+    let duration = time::Duration::from_millis(30);
 
     for x in 1..=100 {
-        thread::sleep(duration);
-
-        let i = mapping(msg_len as f64, 0.0, 100.0, 0.0, x as f64) as i32;
+        let x = ease_in_sine(x as f64 / 100.0);
+        let i = mapping(msg_len as f64, 0.0, 1.0, 0.0, x as f64) as i32;
         if let Some(_) = printed_index.iter().find(|v| **v == i) {
             continue;
         }
@@ -36,6 +35,8 @@ fn print_with_easing(msg: &str) {
             std::io::stdout().flush().unwrap();
             printed_index.push(i);
         }
+
+        thread::sleep(duration);
     }
 
     println!("");
@@ -58,5 +59,11 @@ mod tests {
     fn mapping_works() {
         let result = mapping(500.0, 300.0, 200.0, 100.0, 140.0);
         assert_eq!(result, 380.0);
+    }
+
+    #[test]
+    fn norm_works() {
+        let result = norm(0.01, 1.0, 0.0);
+        assert_eq!(result, 0.01);
     }
 }
